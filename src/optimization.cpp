7eb7bf138d01565db8ebf5f542a6461e9a0cc9b0
Yabin Cui <splintcoder@gmail.com>
2016-03-01 17:12:32 -0800

    Try adding Passes.

--- src/optimization.cpp
+++ src/optimization.cpp
@@ -1,32 +1,59 @@
 #include "optimization.h"
 
+#include <memory>
+
 #include <llvm/Pass.h>
 #include <llvm/PassSupport.h>
 #include <llvm/IR/Function.h>
 #include <llvm/IR/LegacyPassManager.h>
+#include <llvm/IR/Module.h>
 #include <llvm/Transforms/Scalar.h>
 
-/*
-struct HelloPass : public llvm::FunctionPass {
+class HelloModulePass : public llvm::ModulePass {
+ public:
   static char ID;
-  HelloPass() : llvm::FunctionPass(ID) {
+  HelloModulePass() : llvm::ModulePass(ID) {
   }
 
-  bool runOnFunction(llvm::Function& F) override {
-    printf("HelloPass: %s\n", F.getName().data());
+  bool runOnModule(llvm::Module& m) {
+    printf("HelloModulePass: module %s\n", m.getName().data());
     return false;
   }
 };
 
-char HelloPass::ID = 0;
-static llvm::RegisterPass<HelloPass> HelloPassRegister("helloPass", "Hello World
-Pass");
-*/
+char HelloModulePass::ID;
+
+static llvm::RegisterPass<HelloModulePass> HelloModulePassRegister("helloModulePass",
+                                                                   "Hello Module Pass");
+
+class HelloFunctionPass : public llvm::FunctionPass {
+ public:
+  static char ID;
+  HelloFunctionPass() : llvm::FunctionPass(ID) {
+  }
+
+  bool runOnFunction(llvm::Function& F) override {
+    printf("HelloFunctionPass: %s\n", F.getName().data());
+    return false;
+  }
+};
+
+char HelloFunctionPass::ID = 0;
+static llvm::RegisterPass<HelloFunctionPass> HelloFunctionPassRegister("helloFunctionPass",
+                                                                       "Hello Function Pass");
 
 void prepareOptPipeline() {
 }
 
 void optPipeline(llvm::Module* module) {
+  llvm::legacy::PassManager pm;
+  // TODO: who manages Pass instance.
+  llvm::Pass* hello_module_pass = llvm::Pass::createPass(&HelloModulePass::ID);
+  pm.add(hello_module_pass);
+  llvm::Pass* hello_function_pass = llvm::Pass::createPass(&HelloFunctionPass::ID);
+  pm.add(hello_function_pass);
+  pm.run(*module);
+
   llvm::legacy::FunctionPassManager fpm(module);
   fpm.add(llvm::createGVNPass(false));
   fpm.add(llvm::createPromoteMemoryToRegisterPass());
